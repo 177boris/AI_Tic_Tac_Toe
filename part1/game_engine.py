6bb1c1870271ede86257c5e1329c83ab646b418a
177boris <lanreboris11@yahoo.com>
2022-08-14 22:39:15 +0100

    testing render function

--- part1/game_engine.py
+++ part1/game_engine.py
@@ -17,23 +17,29 @@ def new_board():
 
 # Display the board 
 def render(board):
-
     row_num = 0
-    print("   0 1 2 ")
+    print("  0 1 2 ")
     print("  ------")
+    print("")
     for row in board:
         output_row = ""
         for square in row:
             if square is None:
-                output_row += " "
+                output_row += "_"
             else:
                 output_row += square
         print("%d|%s|" % (row_num, ' '.join(output_row)))
         row_num += 1
+    print("")
     print("  ------")
 
 
-board = new_board()
-board[2][1] = "x"
-board[1][1] = "o"
+
+
+board = new_board() # Create new board 
+
+# Test render function 
+board[1][1] = "x"
+board[0][2] = "o"
+board[1][0] = "x"
 render(board)
